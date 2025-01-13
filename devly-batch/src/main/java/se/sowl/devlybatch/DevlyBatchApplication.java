f5abcaf67e92fc7efce374b6d82443970fd42893
Hwasowl <sound2339@naver.com>
2025-01-13 15:33:08 +0900

    feat: 배치 모듈 추가

---
+++ devly-batch/src/main/java/se/sowl/devlybatch/DevlyBatchApplication.java
@@ -0,0 +1,13 @@
+package se.sowl.devlybatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlyBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevlyBatchApplication.class, args);
+    }
+
+}
