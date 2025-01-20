a8f492dc9a6cff5def59f063759bc455b2bae961
Hwasowl <sound2339@naver.com>
2025-01-20 15:34:32 +0900

    feat: FeignClient 타임아웃 대기시간 증가(gpt응답시간 대응)

--- devly-external/src/main/java/se/sowl/devlyexternal/client/config/FeignClientConfig.java
+++ devly-external/src/main/java/se/sowl/devlyexternal/client/config/FeignClientConfig.java
@@ -1,9 +1,12 @@
 package se.sowl.devlyexternal.client.config;
 
+import feign.Request;
 import feign.RequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class FeignClientConfig {
@@ -13,5 +16,16 @@ public class FeignClientConfig {
     @Bean
     public RequestInterceptor bearerTokenRequestInterceptor() {
         return requestTemplate -> requestTemplate.header("Authorization", "Bearer " + apiKey);
+    }
+
+    @Bean
+    public Request.Options feignOptions() {
+        return new Request.Options(
+            60000, // connectTimeout: 30초
+            TimeUnit.MILLISECONDS,
+            60000, // readTimeout: 30초
+            TimeUnit.MILLISECONDS,
+            true   // followRedirects
+        );
     }
 }
