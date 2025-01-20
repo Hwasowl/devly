e5bfa92b91ca45c4b159b7fbefe71703c40c362e
Hwasowl <sound2339@naver.com>
2025-01-20 15:53:39 +0900

    feat: AI 프롬프트 구체화

--- devly-batch/src/main/java/se/sowl/devlybatch/job/word/WordPromptManager.java
+++ devly-batch/src/main/java/se/sowl/devlybatch/job/word/WordPromptManager.java
@@ -6,9 +6,11 @@ public class WordPromptManager {
 
     public static void addDefaultPrompt(Long developerTypeId, StringBuilder prompt) {
         if (developerTypeId.equals(1L)) {
-            prompt.append("스프링 프레임워크 공식 문서를 기반으로 백엔드 개발자를 위한 전문 용어 10개를 생성해주세요. 각 용어는 다음 형식으로 작성하고 용어 구분은 ---를 사용하세요:\n\n");
+            prompt.append("백엔드에 사용되는 언어들 중 공식 문서를 기반으로 백엔드 개발자를 위한 전문 용어 5개를 생성해주세요." +
+                "개발 용어가 아닌(빈, AOP, 스프링 부트) 영어 단어여야 하며, 문서에서 사용되는 각 용어는 다음 형식으로 작성하고 용어 구분은 ---를 사용하세요:\n\n");
         } else if (developerTypeId.equals(2L)) {
-            prompt.append("리액트 공식 문서를 기반으로 프론트엔드 개발자를 위한 전문 용어 10개를 생성해주세요. 각 용어는 다음 형식으로 작성하고 용어 구분은 ---를 사용하세요:\n\n");
+            prompt.append("프론트엔드에 사용되는 언어들 중 공식 문서를 기반으로 프론트엔드 개발자를 위한 전문 용어 5개를 생성해주세요." +
+                "개발 용어가 아닌(컴포넌트, 타입스크립트, 리엑트) 영어 단어여야 하며, 각 용어는 다음 형식으로 작성하고 용어 구분은 ---를 사용하세요:\n\n");
         }
         prompt.append("단어: [영문 용어]\n발음: [발음 기호]\n의미: [한글 의미]\n예문: {\"source\": \"공식 문서 출처\", \"text\": \"영문 예문\", \"translation\": \"한글 번역\"}\n퀴즈: {\"text\": \"\", \"distractors\": [\"오답1\", \"오답2\", \"오답3\", \"오답4\"]}\n---\n");
     }
