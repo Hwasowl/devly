6411f783eb5da4deccb5473ab13ba3092cea0761
Hwasowl <sound2339@naver.com>
2025-01-27 14:07:27 +0900

    feat: 로그인 성공 시 토큰 생성 & 쿠키에 set 설정

--- devly-api/src/main/java/se/sowl/devlyapi/oauth/handler/OAuth2AuthenticationSuccessHandler.java
+++ devly-api/src/main/java/se/sowl/devlyapi/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -2,23 +2,37 @@ package se.sowl.devlyapi.oauth.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import se.sowl.devlyapi.common.jwt.JwtTokenProvider;
+import se.sowl.devlyapi.oauth.dto.TokenResponse;
 
 import java.io.IOException;
+import java.time.Duration;
 
 @Component
+@RequiredArgsConstructor
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-    @Value("${spring.front.url}")
+    private final JwtTokenProvider tokenProvider;
+
+    @Value("${front.url}")
     private String frontUrl;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest req, HttpServletResponse res, Authentication auth) throws IOException {
-        HttpSession session = req.getSession(true);
-         session.setAttribute("user", auth.getPrincipal());
-        getRedirectStrategy().sendRedirect(req, res, frontUrl);
+    public void onAuthenticationSuccess(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        Authentication authentication
+    ) throws IOException {
+        TokenResponse tokenResponse = tokenProvider.createToken(authentication);
+        ResponseCookie cookie = ResponseCookie.from("accessToken", tokenResponse.getAccessToken())
+            .httpOnly(true).secure(true).sameSite("Lax").path("/").maxAge(Duration.ofHours(6)).build();
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+        getRedirectStrategy().sendRedirect(request, response, frontUrl);
     }
 }
--- devly-api/src/main/java/se/sowl/devlyapi/user/controller/UserController.java
+++ devly-api/src/main/java/se/sowl/devlyapi/user/controller/UserController.java
@@ -3,11 +3,12 @@ package se.sowl.devlyapi.user.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.sowl.devlyapi.common.CommonResponse;
+import se.sowl.devlydomain.user.domain.CustomOAuth2User;
+import se.sowl.devlydomain.user.domain.User;
 
 @RestController
 @RequestMapping("/api/users")
@@ -16,8 +17,7 @@ public class UserController {
 
     @GetMapping("/me")
     @PreAuthorize("isAuthenticated()")
-    public CommonResponse<Void> getMe(@AuthenticationPrincipal OAuth2User user) {
-        System.out.println(user.toString());
-        return CommonResponse.ok();
+    public CommonResponse<User> getMe(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+        return CommonResponse.ok(customOAuth2User.getUser());
     }
 }
