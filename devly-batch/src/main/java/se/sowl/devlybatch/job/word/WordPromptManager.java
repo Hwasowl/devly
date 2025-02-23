0c14e66e83438efcf21a17510c294a9f131c0efb
Hwasowl <sound2339@naver.com>
2025-02-23 12:43:19 +0900

    feat: 단어 생성 프롬프트 정확도 개선

--- devly-batch/src/main/java/se/sowl/devlybatch/job/word/WordPromptManager.java
+++ devly-batch/src/main/java/se/sowl/devlybatch/job/word/WordPromptManager.java
@@ -7,12 +7,14 @@ public class WordPromptManager {
     public static void addDefaultPrompt(Long developerTypeId, StringBuilder prompt) {
         if (developerTypeId.equals(1L)) {
             prompt.append("백엔드에 사용되는 언어들 중 공식 문서를 기반으로 백엔드 개발자를 위한 전문 용어 5개를 생성해주세요." +
+                "문서에서 사용되는 단어가 다른 형식으로 사용된다면 유연하게 처리해주세요. dependency -> dependencies 로 사용 된 경우 곤란해집니다." +
                 "개발 용어가 아닌(빈, AOP, 스프링 부트) 영어 단어여야 하며, 문서에서 사용되는 각 용어는 다음 형식으로 작성하고" +
                 "의미를 정확하게 이해할 수 있도록 묘사해주세요. 또한 translation 값도 정확하게 묘사해주세요." +
                 "quiz는 단어를 맞추는 형식으로 생성한 단어가 들어간 문장을 만들어주세요. 빈칸 처리는 프론트에서 처리합니다." +
                 "용어 구분은 ---를 사용하세요:\n\n");
         } else if (developerTypeId.equals(2L)) {
             prompt.append("프론트엔드에 사용되는 언어들 중 공식 문서를 기반으로 프론트엔드 개발자를 위한 전문 용어 5개를 생성해주세요." +
+                "문서에서 사용되는 단어가 다른 형식으로 사용된다면 유연하게 처리해주세요. dependency -> dependencies 로 사용 된 경우 곤란해집니다." +
                 "개발 용어가 아닌(컴포넌트, 타입스크립트, 리엑트) 영어 단어여야 하며, 각 용어는 다음 형식으로 작성하고 " +
                 "의미를 정확하게 이해할 수 있도록 묘사해주세요. 또한 translation 값도 정확하게 묘사해주세요." +
                 "quiz는 단어를 맞추는 형식으로 생성한 단어가 들어간 문장을 만들어주세요. 빈칸 처리는 프론트에서 처리합니다." +
