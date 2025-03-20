7d6721aceba685b51125d7e334670d98b8eabd49
Hwasowl <sound2339@naver.com>
2025-03-20 19:20:09 +0900

    feat: JPA Config 도메인 모듈로 책임 이전

--- devly-domain/src/main/java/se/sowl/devlydomain/config/JpaConfig.java
+++ devly-domain/src/main/java/se/sowl/devlydomain/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package se.sowl.devlyapi.common.config;
+package se.sowl.devlydomain.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
