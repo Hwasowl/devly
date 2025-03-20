22696b882c009c3926be387fdcef184be2c5d02b
Hwasowl <sound2339@naver.com>
2025-03-20 19:24:16 +0900

    fix: external applications entity scan missing domain package

--- devly-external/src/main/java/se/sowl/devlyexternal/DevlyExternalApplication.java
+++ devly-external/src/main/java/se/sowl/devlyexternal/DevlyExternalApplication.java
@@ -2,14 +2,16 @@ package se.sowl.devlyexternal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import se.sowl.devlydomain.config.JpaConfig;
 import se.sowl.devlyexternal.client.config.FeignConfig;
 
 @SpringBootApplication
-@Import({FeignConfig.class, JpaConfig.class})
+@EntityScan(basePackages = {"se.sowl.devlydomain"})
 @ComponentScan(basePackages = {"se.sowl.devlydomain", "se.sowl.devlyexternal"})
+@Import({FeignConfig.class, JpaConfig.class})
 public class DevlyExternalApplication {
 
 	public static void main(String[] args) {
